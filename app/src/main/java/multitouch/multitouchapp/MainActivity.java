cdd52ee018190c2fc00a206cc9e1bcece01daa28
Lauren Winston <lwinston@gatech.edu>
2016-10-31 01:18:52 -0400

    Added buttons to file menu row

--- app/src/main/java/multitouch/multitouchapp/MainActivity.java
+++ app/src/main/java/multitouch/multitouchapp/MainActivity.java
@@ -2,6 +2,8 @@ package multitouch.multitouchapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import multitouch.test.R;
 
@@ -11,5 +13,13 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void Open(View view) {
+        // Open a new file
+    }
+
+    public void Save(View view) {
+        // Save file
     }
 }
