74cf67130d768bcae9a2c72ad5926166a808cdb1
Lauren Winston <lwinston@gatech.edu>
2016-10-31 01:47:22 -0400

    Basic stroke detection

--- app/src/main/java/multitouch/multitouchapp/DrawingView.java
+++ app/src/main/java/multitouch/multitouchapp/DrawingView.java
@@ -3,7 +3,10 @@ package multitouch.multitouchapp;
 import android.content.Context;
 import android.graphics.Canvas;
 import android.graphics.Paint;
+import android.support.v4.view.MotionEventCompat;
 import android.util.AttributeSet;
+import android.util.Log;
+import android.view.MotionEvent;
 import android.view.View;
 
 /**
@@ -16,11 +19,37 @@ public class DrawingView extends View{
     public DrawingView(Context context, AttributeSet attrs) {
         super(context, attrs);
         mPaint = new Paint();
+        this.setOnTouchListener(new BasicStrokeListener());
     }
 
 
     @Override
     public void onDraw(Canvas canvas) {
 
+    }
+
+    private class BasicStrokeListener implements OnTouchListener {
+
+        @Override
+        public boolean onTouch(View v, MotionEvent event) {
+            int action = MotionEventCompat.getActionMasked(event);
+
+            switch(action) {
+                case (MotionEvent.ACTION_DOWN) :
+                    // Create stroke
+                    return true;
+                case (MotionEvent.ACTION_MOVE) :
+                    // Update stroke
+                    return true;
+                case (MotionEvent.ACTION_UP) :
+                    // Finish stroke
+                    return true;
+                case (MotionEvent.ACTION_OUTSIDE) :
+                    // Finish stroke
+                    return true;
+                default :
+                    return false;
+            }
+        }
     }
 }
